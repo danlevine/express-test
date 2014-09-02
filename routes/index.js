c7243cb5889710bb7c6c464ae522e3e768b08cc3
Daniel Levine <dlevine@marketwiise.com>
2014-09-02 14:33:11 -0500

    Added contact route and further styling

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Jamrock'});
+  res.render('index', { title: 'Bread'});
 });
 
 router.get('/about', function(req, res) {
   res.render('about', { title: 'About'});
+});
+
+router.get('/contact', function(req, res) {
+  res.render('contact', { title: 'Contact'});
 });
 
 module.exports = router;

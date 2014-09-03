7742308a72c61c8f6d3c2eb86b0cfc2f0a8da1f3
Daniel Levine <dlevine@marketwiise.com>
2014-09-03 10:26:08 -0500

    Flickr gallery hookup CP

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
+var request = require('request');
 var express = require('express');
 var router = express.Router();
+var _ = require('lodash');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -15,7 +17,32 @@ router.get('/contact', function(req, res) {
 });
 
 router.get('/gallery', function(req, res) {
-  res.render('gallery', { title: 'Gallery'});
+  var options = {
+    url: 'https://api.flickr.com/services/feeds/photos_public.gne/?format=json&nojsoncallback=1',
+    json: false
+  }
+
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+
+      console.log(body);
+      // console.log(content.items);
+      console.log(body.modified);
+
+      var content = JSON.parse(body);
+      console.log(content.modified);
+
+      var itemURLs = [];
+
+      _.forEach(content, function(item) {
+        itemURLs.push(item.media.m);
+      });
+
+      // console.log(itemURLs);
+
+      res.render('gallery', { title: 'Gallery', itemURLs: itemURLs});
+    }
+  })
 });
 
 module.exports = router;
